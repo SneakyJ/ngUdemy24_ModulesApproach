f85cae1ae577f070cc3ec854334587b71930dadf
Kostiantyn Nikolaienko <kostiantyn.nikolaienko@siemens-healthineers.com>
2024-12-21 21:44:27 +0100

    Outputting user-specific tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
@@ -8,10 +8,36 @@ import { TaskComponent } from "./task/task.component";
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
-
-// @Input({required: true}) name! - (!) tells TS that we ENSURE that name property WILL NEVER BE 'undefined'
-// @Input() name? - (?) tells TS that we AWARE that name property CAN BE 'undefined'
-
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  dummyTasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ]
+
+  get selectedUserTasks() {
+    return this.dummyTasks.filter((task) => task.userId === this.userId );
+  }
 }
