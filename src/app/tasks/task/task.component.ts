8104a8db1a308bb90ca9c0cc133aed11d6813bc2
Kostiantyn Nikolaienko <kostiantyn.nikolaienko@siemens-healthineers.com>
2024-12-22 17:19:21 +0100

    Outputting task data in task template

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task',
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
+  @Input({required: true}) task!: Task;
 
+}
+
+interface Task {
+  id: string,
+  userId: string,
+  title: string,
+  summary: string,
+  dueDate: string
 }
