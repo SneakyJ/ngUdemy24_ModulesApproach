3daa6661ceee590a29344e8807f8fd7f987b3376
SneakyJ <k.nikolaienko@gmail.com>
2024-12-26 23:37:56 +0100

    New task creation functionality added

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,8 +1,10 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../../models/task.model';
 
 @Component({
   selector: 'app-new-task',
+  standalone: true,
   imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
@@ -10,11 +12,20 @@ import { FormsModule } from '@angular/forms';
 
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
 
   onCancel(){
     this.cancel.emit();
+  }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate
+    });
   }
 }
