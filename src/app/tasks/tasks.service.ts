23130370a7a546f68e3984dc4f2cfbd9a699a96b
SneakyJ <k.nikolaienko@gmail.com>
2024-12-31 00:06:41 +0100

    Adding saving tasks to local storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { NewTaskData } from '../models/task.model';
+import { JsonPipe } from '@angular/common';
 
 @Injectable({providedIn: 'root'})
 export class TasksService {
@@ -30,6 +31,15 @@ export class TasksService {
     },
   ]
 
+  constructor (){
+    const tasks = localStorage.getItem('tasks');
+
+    if(tasks){
+        this.dummyTasks = JSON.parse(tasks);
+    }
+
+  }
+
   getUserTasks(userId: string){
     return this.dummyTasks.filter((task) => task.userId === userId );;
   }
@@ -42,9 +52,15 @@ export class TasksService {
         summary: taskData.summary,
         dueDate: taskData.date
       });
+      this.saveTasks();
   }
 
   removeTask(id: string){
     this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks(){
+    localStorage.setItem('tasks', JSON.stringify(this.dummyTasks))
   }
 }
