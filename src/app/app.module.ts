6342be4c90d653a4270b886f7df9a942bac8533a
SneakyJ <k.nikolaienko@gmail.com>
2025-01-02 00:06:02 +0100

    Migrate project and its componentns from mixed approach to Modules approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,21 @@ import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from "./tasks/tasks.component";
 import { CardComponent } from "./shared/card/card.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { FormsModule } from "@angular/forms";
+import { TaskComponent } from "./tasks/task/task.component";
 
 @NgModule({
-    declarations: [AppComponent],
-    bootstrap: [AppComponent],
-    imports: [BrowserModule,
+    declarations: [AppComponent,
         HeaderComponent, 
         UserComponent,
         CardComponent, 
-        TasksComponent]
+        TasksComponent,
+        TaskComponent,
+        NewTaskComponent],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule, FormsModule
+        ]
 })
 export class AppModule {
 
