89353e509e4c151fc8cbe96cfede4e439a121f40
SneakyJ <k.nikolaienko@gmail.com>
2025-01-01 23:52:00 +0100

    Applying ng  Modules approach combined with standalone approach

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { CardComponent } from "./shared/card/card.component";
+
+@NgModule({
+    declarations: [AppComponent],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule,
+        HeaderComponent, 
+        UserComponent,
+        CardComponent, 
+        TasksComponent]
+})
+export class AppModule {
+
+}
